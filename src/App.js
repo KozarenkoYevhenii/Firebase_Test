525cad79a497cc325bc21288b6962b598d15bdef
Yevhenii Kozarenko <kozarenk007@gmail.com>
2020-12-14 22:50:13 +0200

    create firebase realtime databese and connect it to project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
 import "./App.css";
 import Login from "./components/login/Login";
 import Register from "./components/register/Register";
+import firebase from "firebase";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Route path="/register" component={Register} />
-      <Route path="/login" component={Login} />
-      <Route exact path="/" component={Login} />
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    const db = firebase.database();
+    console.log(db);
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <Route path="/register" component={Register} />
+        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Login} />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAezqBb5QeeN4k6Prd8MFukNR_qqJ3rAr8",
+  authDomain: "fir-test-e1e26.firebaseapp.com",
+  databaseURL: "https://fir-test-e1e26-default-rtdb.firebaseio.com",
+  projectId: "fir-test-e1e26",
+  storageBucket: "fir-test-e1e26.appspot.com",
+  messagingSenderId: "971799009249",
+  appId: "1:971799009249:web:03d899434899aae569a4a0",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
